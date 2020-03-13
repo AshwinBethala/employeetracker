9b4f71857751c053d06a1bddd49b6158275a5c64
Ashwin Bethala <57239378+AshwinBethala@users.noreply.github.com>
2020-03-12 23:35:48 -0500

    Add files via upload

---
+++ seed.sql
@@ -0,0 +1,16 @@
+select * from employee;
+select * from role;
+select * from department;
+
+INSERT into department (name)
+VALUES ("Sales");
+INSERT into department (name)
+VALUES ("Engineering");
+INSERT into department (name)
+VALUES ("Finance");
+INSERT into department (name)
+VALUES ("Legal");
+INSERT into department (name)
+VALUES ("Manager");
+
+select * from department;
